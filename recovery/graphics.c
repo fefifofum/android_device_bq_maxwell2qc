2fdbfe84eaebca4d02a8627a5fb300b52a020b52
fefifofum <jorsellis@gmail.com>
2014-06-03 21:54:06 +0200

    Invert screen for recovery

--- recovery/graphics.c
+++ recovery/graphics.c
@@ -293,6 +293,18 @@ void gr_flip(void)
         if (double_buffering)
             gr_active_fb = (gr_active_fb + 1) & 1;
 
+        /* flip buffer 180 degrees for devices with physicaly inverted screens */
+        unsigned int i;
+        unsigned int j;
+        uint8_t tmp;
+        for (i = 0; i < ((vi.xres_virtual * vi.yres)/2); i++) {
+	    for (j = 0; j < PIXEL_SIZE; j++) {
+		    tmp = gr_mem_surface.data[i * PIXEL_SIZE + j];
+		    gr_mem_surface.data[i * PIXEL_SIZE + j] = gr_mem_surface.data[(vi.xres_virtual * vi.yres * PIXEL_SIZE) - ((i+1) * PIXEL_SIZE) + j];
+		    gr_mem_surface.data[(vi.xres_virtual * vi.yres * PIXEL_SIZE) - ((i+1) * PIXEL_SIZE) + j] = tmp;
+	    }
+        }
+
         /* copy data from the in-memory surface to the buffer we're about
          * to make active. */
         memcpy(gr_framebuffer[gr_active_fb].data, gr_mem_surface.data,
